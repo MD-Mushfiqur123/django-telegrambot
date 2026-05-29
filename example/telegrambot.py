7c4f4ece3a04938cc634a759d155bd470509db18
Md Mushfiqur Rahim <20mahin2020@gmail.com>
2026-05-29 08:55:06 +0600

    fix: replace deprecated logger.warn with logger.warning
    logger.warn() is deprecated since Python 3.3 and emits a DeprecationWarning.
    Use logger.warning() instead.
    
    Signed-off-by: god032396-del <god032396@gmail.com>

---
+++ example/telegrambot.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Example code for telegrambot.py module
+from telegram.ext import CommandHandler, MessageHandler, Filters
+from django_telegrambot.apps import DjangoTelegramBot
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    context.bot.sendMessage(update.message.chat_id, text='Hi!')
+
+
+def help(update, context):
+    context.bot.sendMessage(update.message.chat_id, text='Help!')
+
+
+def echo(update, context):
+    context.bot.sendMessage(update.message.chat_id, text=update.message.text)
+
+
+def error(update, context, error):
+    logger.warning('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    logger.info("Loading handlers for telegram bot")
+
+    # Default dispatcher (this is related to the first bot in settings.TELEGRAM_BOT_TOKENS)
+    dp = DjangoTelegramBot.dispatcher
+    # To get Dispatcher related to a specific bot
+    # dp = DjangoTelegramBot.getDispatcher('BOT_n_token')     #get by bot token
+    # dp = DjangoTelegramBot.getDispatcher('BOT_n_username')  #get by bot username
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler([Filters.text], echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # log all errors
+    dp.addErrorHandler(error)
